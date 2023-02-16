664aa9bb9e50113d390d4fe40a1cdc701dc192fe
Tinmarian <tinmar96@gmail.com>
2023-02-16 10:21:55 -0600

    Last commit in first homework

--- 1.1_user_processing.py
+++ 1.1_user_processing.py
@@ -4,12 +4,16 @@ from datetime import datetime
 
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 
+default_args = {
+    "owner":"Tinmar"
+}
+
 with DAG(
             '1.1_user_processing',
             start_date=datetime(2023,2,16),
             schedule_interval='@daily',
             catchup=False,
-            owner='Tinmar',
+            default_args=default_args,
             tags=['Curso 3', 'Introduction_to_Apache_Airflow']
         ) as dag:
     None
