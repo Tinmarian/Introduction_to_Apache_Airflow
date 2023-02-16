6a09d81ff76e8157590d677f2428a0333fce8e7a
Tinmarian <tinmar96@gmail.com>
2023-02-16 09:27:47 -0600

    Adding owner

--- 1.2_user_processing.py
+++ 1.2_user_processing.py
@@ -10,6 +10,7 @@ with DAG(
             start_date=datetime(2023,2,16),
             schedule_interval='@daily',
             catchup=False,
+            owner='Tinmar',
             tags=['Curso 3', 'Introduction_to_Apache_Airflow']
         ) as dag:
     
