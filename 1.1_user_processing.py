2a29257791f9259c268deee2e39244ba50ac8ad2
Tinmarian <tinmar96@gmail.com>
2023-02-16 09:01:12 -0600

    Changes in the first homework

--- 1.1_user_processing.py
+++ 1.1_user_processing.py
@@ -2,11 +2,13 @@ from airflow.models import DAG
 
 from datetime import datetime
 
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
 with DAG(
-            '1_user_processing',
+            '1.1_user_processing',
             start_date=datetime(2023,2,16),
             schedule_interval='@daily',
             catchup=False,
             tags=['Curso 3', 'Introduction_to_Apache_Airflow']
         ) as dag:
-    
+    None
