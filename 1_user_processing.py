7d8c315055bb7c58e6bcbb1008553f020fe63752
Tinmarian <tinmar96@gmail.com>
2023-02-16 08:43:12 -0600

    Changing dag_id in line 6

--- 1_user_processing.py
+++ 1_user_processing.py
@@ -3,7 +3,7 @@ from airflow.models import DAG
 from datetime import datetime
 
 with DAG(
-            'user_processing',
+            '1_user_processing',
             start_date=datetime(2023,2,16),
             schedule_interval='@daily',
             catchup=False,
