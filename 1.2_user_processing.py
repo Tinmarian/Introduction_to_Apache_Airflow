6b0443ef84d31a3411164536e4666afb0bd8700e
Tinmarian <tinmar96@gmail.com>
2023-02-16 10:21:23 -0600

    Last commit of second homework

--- 1.2_user_processing.py
+++ 1.2_user_processing.py
@@ -5,12 +5,16 @@ from datetime import datetime
 from airflow.operators.dummy import DummyOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 
+default_args={
+    "owner":"Tinmar"
+}
+
 with DAG(
             '1.2_user_processing',
             start_date=datetime(2023,2,16),
             schedule_interval='@daily',
             catchup=False,
-            owner='Tinmar',
+            default_args=default_args,
             tags=['Curso 3', 'Introduction_to_Apache_Airflow']
         ) as dag:
     
@@ -18,7 +22,7 @@ with DAG(
 
     create_table = PostgresOperator(
                                     task_id='create_table',
-                                    postgres_conn_id = 'postgres',
+                                    postgres_conn_id='postgres',
                                     sql="""
                                         CREATE TABLE IF NOT EXISTS users (
                                             firstname TEXT NOT NULL,
@@ -29,6 +33,22 @@ with DAG(
                                             email TEXT NOT NULL
                                         );
                                     """
+                                )
+
+    insert_data = PostgresOperator(
+                                    task_id='insert_data',
+                                    postgres_conn_id='postgres',
+                                    sql= """
+                                        INSERT INTO users VALUES ('Tinmar', 'Andrade','México','tinmar','GuruSat.3','tinmar96@gmail.com')
+                                    """  
+                                )
+
+    select_data = PostgresOperator(
+                                    task_id='select_data',
+                                    postgres_conn_id='postgres',
+                                    sql="""
+                                        SELECT * FROM users
+                                    """   
                                 )
     
     end = DummyOperator(task_id='end')
